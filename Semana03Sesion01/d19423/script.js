73421987ac2f28d30d1c04847b661a2f012a8bfb
Rosita <rosa00nc@gmail.com>
2023-11-30 19:57:38 -0500

    actualizar

--- Semana03Sesion01/d19423/script.js
+++ Semana03Sesion01/d19423/script.js
@@ -1,1 +1,63 @@
-console.log("Hola desde el archivo")
+var nombre = Rosita 
+console.log(nombre)
+    
+    nombre = "Karen"
+    console.log(nombre)
+
+    var primerNombre = "David" //para hacer comentarios
+
+    var primerApellido= "Lopez"
+    var segundo_apellido= "Pineda"
+    let numeroDni="00000000"
+
+    {let numeroDni='11111111'}
+
+
+     const PI=3.1416;
+    console.error(PI);
+    
+    console.info(PI);
+
+//OPERACIONES
+let x=5;
+let y=2;
+let z= x*y;
+console.log(z);
+
+let er=10;
+er +=5;
+
+console.log(er);
+
+// operadores de comparacion
+
+let comp=13==15;
+console.log(comp);
+
+let tvSala="55"
+let tvDormitorio=55;
+let iguales = tvSala==tvDormitorio;
+
+console.log(iguales);
+
+iguales=tvSala===tvDormitorio;
+console.log(iguales);
+
+//Tipos de Datos
+
+//Palabras o secuencias de palabras, caracteres STRINGS
+let caracteres="Palabra";
+let caracteres2=' Palabra 2';
+let caracteres3= 'Palabra 3 es la mejor opcion para escribir';
+console.log(caracteres3);
+
+let caracteres4= ' Hola mi nombre es ${nombre}';
+console.log(caracteres4)
+
+let caracteres5=" Hola mi nombre es "+ nombre;
+console.log(caracteres5);
+
+let text ="ABCDRF";
+let length=text. length;
+console.log(length);
+
