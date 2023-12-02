240ba3d57a21586f521c26efdd15c80b538b6574
Gustavo Chacón <gchacon1987@gmail.com>
2023-12-02 08:43:08 -0500

    creacion de botones

---
+++ Hackaton03/A20004187/script.js
@@ -0,0 +1,175 @@
+function ejercicio01(){
+    //Hacer un algoritmo en JavaScript que lea un número por el teclado y 
+    //determinar si tiene tres dígitos.
+    let numero = parseInt( prompt("Escribe un numero"))
+    if(numero>100 && numero< 999){
+        alert("Este numero tiene 3 digitos")
+    }
+    else{
+        alert("Este numero no tiene 3 digitos")
+    }
+    }
+
+function ejercicio02(){
+    //Hacer un algoritmo en JavaScript que lea un número por el teclado y 
+    //determinar si es negativo.
+    let numero = parseInt( prompt("Escribe un numero"))
+    if(numero<0){
+        alert("Este numero es negativo")
+    }
+    else{
+        alert("Este numero no es negativo")
+    }
+    }
+
+function ejercicio03(){
+
+}
+
+function ejercicio04(){
+
+}
+
+function ejercicio05(){
+
+}
+
+function ejercicio06(){
+
+}
+
+function ejercicio07(){
+
+}
+
+function ejercicio08(){
+
+}
+
+function ejercicio09(){
+
+}
+
+function ejercicio10(){
+
+}
+
+function ejercicio11(){
+
+}
+
+function ejercicio12(){
+
+}
+
+function ejercicio13(){
+
+}
+
+function ejercicio14(){
+
+}
+
+function ejercicio15(){
+
+}
+
+function ejercicio16(){
+
+}
+
+function ejercicio17(){
+
+}
+
+function ejercicio18(){
+
+}
+
+function ejercicio19(){
+
+}
+
+function ejercicio20(){
+
+}
+
+function ejercicio21(){
+
+}
+
+function ejercicio22(){
+
+}
+
+function ejercicio23(){
+
+}
+
+function ejercicio24(){
+
+}
+
+function ejercicio25(){
+
+}
+
+function ejercicio26(){
+
+}
+
+function ejercicio27(){
+
+}
+
+function ejercicio28(){
+
+}
+
+function ejercicio29(){
+
+}
+
+function ejercicio30(){
+
+}
+
+function ejercicio31(){
+
+}
+
+function ejercicio32(){
+
+}
+
+function ejercicio33(){
+
+}
+
+function ejercicio34(){
+
+}
+
+function ejercicio35(){
+
+}
+
+function ejercicio36(){
+
+}
+
+function ejercicio37(){
+
+}
+
+function ejercicio38(){
+
+}
+
+function ejercicio39(){
+
+}
+
+function ejercicio40(){
+
+}
