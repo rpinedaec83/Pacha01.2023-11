e74aeed73379225d3e6a72ca4a1de1558aadb1eb
Gustavo Chacón <gchacon1987@gmail.com>
2023-12-16 23:14:26 -0500

    hackaton 4 completo

--- Hackaton04/A20004187/script.js
+++ Hackaton04/A20004187/script.js
@@ -124,9 +124,36 @@ function ejercicio02(){
     function ejercicioR2_03(){
     //Crear una funcion que me retorne el tipo de valor entregado, 
     //invocar la función para los distintos tipos de js
+    function TipoValor( v1,v2,v3,v4,v5,v6){
+        return typeof v1;
+        return typeof v2;
+        return typeof v3;
+        return typeof v4;
+        return typeof v5;
+        return typeof v6;
+      };
+      let dato1 = "Gustavo";
+      let dato2 = 33;
+      let dato3 = true;
+      let dato4 = null;
+      let dato5;
+      let dato6 = {
+        name: "Aldo",
+        lastname: "Chacón"
+      };
+    alert(`El tipo de valor de "${dato1}" es ${TipoValor(dato1)}`);
+    alert(`El tipo de valor de "${dato2}" es ${TipoValor(dato2)}`);
+    alert(`El tipo de valor de "${dato3}" es ${TipoValor(dato3)}`);
+    alert(`El tipo de valor de "${dato3}" es ${TipoValor(dato4)}`);
+    alert(`El tipo de valor de "${dato5}" es ${TipoValor(dato5)}`);
+    alert(`El tipo de valor de "${dato6}" es ${TipoValor(dato6)}`);
     }
     function ejercicioR2_04(){
     //Crear una función que reciba n cantidad de argumentos y los sume ( utilizar parametros rest)
+    const sumarArgumentos = (...args)=>{
+        return args.reduce((suma,arg)=>suma+arg,0)
+        }
+        alert(sumarArgumentos(1,2,3,4,5))
     }
     function ejercicioR2_05(){
     //Crear una función que reciba un array de valores y filtre los valores que no son string
@@ -161,23 +188,55 @@ function ejercicio02(){
     //Cree una función que tome una matriz de matrices con números. 
     //Devuelve una nueva matriz (única) con el mayor número de cada uno.
     //findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) ➞ [7, 90, 2]
-
+    function matrizMayor(a1,a2,a3){
+        m1 = Math.max(...a1);
+        m2 = Math.max(...a2);
+        m3 = Math.max(...a3);
+        let mT = [m1,m2,m3]
+        return mT;
+    }
+        
+        let arr1=[];
+        let arr2=[];
+        let arr3=[];
+    
+        for(i=0;i<4;i++){
+        arr1[i]=prompt(`Ingrese el elemento ${i} de la matriz 1`);
+    }
+        for(i=0;i<4;i++){
+        arr2[i]=prompt(`Ingrese el elemento ${i} de la matriz 2`);
+    }
+        for(i=0;i<4;i++){
+        arr3[i]=prompt(`Ingrese el elemento ${i} de la matriz 3`);
+    }
+        alert(`La matriz con los numeros mayores es [${matrizMayor(arr1,arr2,arr3)}]`);
     }
     function ejercicioR2_09(){
     //Dada una palabra, escriba una función que devuelva el primer índice y el último índice de un carácter.
     
     //charIndex("hello", "l") ➞ [2, 3]
     // The first "l" has index 2, the last "l" has index 3.
-
     //charIndex("circumlocution", "c") ➞ [0, 8]
     // The first "c" has index 0, the last "c" has index 8.
-
+    function primerUltimo(palabra){
+        const array = palabra.split('');
+        return "El primer es "+ array[0]+" y el ultimo "+array[array.length-1];
+    }
+    pal=prompt("Ingrese una palabra");
+    alert(primerUltimo(pal));
     }
     function ejercicioR2_10(){
     //Escriba una función que convierta un objeto en una matriz, 
     //donde cada elemento representa un par clave-valor.
     //toArray({ a: 1, b: 2 }) ➞ [["a", 1], ["b", 2]]
-
+    const obj1={
+        a:1,
+        b:2
+        }
+        
+        const toArray = (object) => Object.entries(object)
+        
+        console.log(toArray(obj1));
     }
     function ejercicioR2_11(){
     //Cree la función que toma una matriz con objetos y 
@@ -188,7 +247,21 @@ function ejercicio02(){
     //{ name: "Steve",  age: 32, budget: 40000 },
     //{ name: "Martin",  age: 16, budget: 2700 }
     //]) ➞ 65700
-
+    function presupuesto(a){
+        return a[0].budget+a[1].budget+a[2].budget
+        }
+          
+        let arr = [{
+                name:"John",
+                age:21,
+                budget:23000},{
+                name:"Steve",
+                age:32,
+                budget:40000},{
+                name:"Martin",
+                age:16,
+                budget:2700}]
+        alert(`La suma de los presupuestos es ${presupuesto(arr)}`);
     }
     function ejercicioR2_12(){
     //Cree una función que tome una matriz de estudiantes y devuelva una matriz de nombres de estudiantes.
@@ -197,7 +270,12 @@ function ejercicio02(){
     //{ name: "Mike" },
     //{ name: "John" }
     //]) ➞ ["Becky", "John", "Steve"]
-
+    const students =[
+        {name: "Steve"},
+        {name: "Mike"},
+        {name: "John"}
+        ]
+        alert(students.map((student,index)=>{return student.name}))
     }
     function ejercicioR2_13(){
     //Escriba una función que convierta un objeto en una matriz de claves y valores.
@@ -206,7 +284,14 @@ function ejercicio02(){
     //dislikes: 3,
     //followers: 10
     //}) ➞ [["likes", 2], ["dislikes", 3], ["followers", 10]]
-
+    const obj1={
+        likes: 2,
+        dislikes: 3,
+        followers: 10
+        }
+        const toArray = (object) => Object.entries(object)
+        
+        console.log(toArray(obj1));
     }
     function ejercicioR2_14(){
     //Cree una función donde, dado el número n, devuelva la suma de todos los números cuadrados  incluyendo n.
@@ -214,6 +299,16 @@ function ejercicio02(){
     // 1² + 2² + 3² =
     // 1 + 4 + 9 =
     // 14
+    function sumaCuadrados(n){
+        let sumaC=0;
+        for(i=1;i<=n;i++){
+            sumaC+=Math.pow(i,2);
+            }
+            return sumaC;
+    }
+   
+    n=parseInt(prompt("Ingrese un numero"))
+    alert(sumaCuadrados(n));
     }
     function ejercicioR2_15(){
     //Cree una función para multiplicar todos los valores en una matriz 
@@ -229,6 +324,10 @@ function ejercicio02(){
     //Cree una función que tome un número como argumento y 
     //devuelva una matriz de números contando desde este número a cero.
     //countdown(5) ➞ [5, 4, 3, 2, 1, 0]
+    const contarArray = (number) =>{
+        return Array.from({length: number+1},(_,i)=>number-i)
+        }
+        console.log(contarArray(4))
     }
     function ejercicioR2_17(){
     //Cree una función que tome una matriz y devuelva la diferencia entre 
@@ -266,10 +365,12 @@ function ejercicio02(){
     //mientras que el segundo argumento (veces) es la cantidad de veces que se debe repetir el elemento. 
     //Devuelve el resultado en una matriz.
     //repeat(13, 5) ➞ [13, 13, 13, 13, 13]
-    const repeat = (elemento,veces)=>{
-        return Array.from({length: veces}, () => elemento)
+    const matrizD = (e,v)=>{
+        return Array.from({length: v}, () => e)
     }
-    alert(repeat(13,5));
+    let elem = parseInt(prompt("Ingrese un numero elemento"));
+    let veces = parseInt(prompt("Ingrese un numero de veces"));
+    alert(`[${matrizD(elem,veces)}]`);
     }
     function ejercicioR2_20(){
     //Escriba una función, .vreplace () que extienda el prototipo de cadena 
@@ -290,21 +391,21 @@ function ejercicio02(){
     const cadena = `I am finding Nemo !`;
 
     const encontarNemo = (cadenaTexto) =>{
-        const ArrayPalabras = cadenaTexto.split(``)
-        console.log(ArrayPalabras);
-        const posicionNemo = ArrayPalabras.indexOf(`N`)
-        console.log(posicionNemo);
+        const ArrayPalabras = cadenaTexto.split('')
+      
+        const posicionNemo = ArrayPalabras.indexOf('Nemo')
         return posicionNemo + 1;
+        
     }
-    console.log(`Encontre a nemo en la posicion ${encontarNemo(cadena)}`);
+    alert(`Encontre a nemo en la posicion ${encontarNemo(cadena)}`);
     }
     function ejercicioR2_22(){
     //Cree una función que capitalice la última letra de cada palabra.
     //capLast("hello") ➞ "hellO"
     const capLast = (word) =>{
-        return word.split("").map(letter=>letter.slice(0,-1)+letter.slice(-1).toUpperCase()).join(``);
+        return word.split().map(letter=>letter.slice(0,-1)+letter.slice(-1).toUpperCase()).join(``);
         
     }
-    console.log(capLast("hola"));
+    alert(capLast("hola"));
     }
     
