d753675928e63d1c00f425522ceba5130c7fc358
Rosita <rosa00nc@gmail.com>
2023-12-19 20:03:15 -0500

    semana06

---
+++ semana06/rosita/script.js
@@ -0,0 +1,9 @@
+let titulo=document.getElementById("titulo");
+console.log(titulo);
+titulo.innerText="este es mi titulo";
+let h1Pagina=document.getElementById("h1Pagina");
+h1Pagina.innerText="Hola"
+
+
+let div1=document.getElementById("div1");
+div1.innerHTML='<p>Mi parrafo</p>'
