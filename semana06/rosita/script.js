def81552cc0c03d8cfb3657c313cb5c22401fc28
Rosita <rosa00nc@gmail.com>
2023-12-21 20:48:00 -0500

    Merge remote-tracking branch 'fuentes/main' into develop

--- semana06/rosita/script.js
+++ semana06/rosita/script.js
@@ -1,9 +1,13 @@
-let titulo=document.getElementById("titulo");
-console.log(titulo);
-titulo.innerText="este es mi titulo";
-let h1Pagina=document.getElementById("h1Pagina");
-h1Pagina.innerText="Hola"
+if(typeof(storage)!=="undefined"){
+    console.log("si se puede")
 
 
-let div1=document.getElementById("div1");
-div1.innerHTML='<p>Mi parrafo</p>'
+sessionStorage.setItem("nombre","Rosa")
+
+
+
+}else{
+
+console.log("sorry")
+
+}
