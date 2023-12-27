930aa41b44b88f846ee5e7beaa22e3bca578bb52
Gustavo Chacón <gchacon1987@gmail.com>
2023-12-27 15:35:41 -0500

    hackaton5 terminado

--- Hackaton05/A20004187/script.js
+++ Hackaton05/A20004187/script.js
@@ -5,19 +5,22 @@ const Reparaciones = function () {
 
     function eventos() {
         document.getElementById("ingresoTelefono").addEventListener("click", ingresarTelefono);
+        document.getElementById("diagnostico").addEventListener("click",diagnostico);
+        document.getElementById("myCheck").addEventListener("click",Abono)
+        document.getElementById("autorizacion").addEventListener("click",confirmacion);
+        document.getElementById("finalizado").addEventListener("click",terminado);
     }
     function ingresarTelefono() {
         let IMEI = prompt("Ingresa el IMEI");
         let nuevoTelefono = new Telefono(IMEI);
         nuevoTelefono.verificarIMEI();
-
+        document.getElementById("estado").innerHTML = "Inicio";
     }
-    return {
+   return {
         init: function (parametros) {
             Nombre = parametros.Nombre;
             Ubicacion = parametros.Ubicacion;
             eventos();
-
         },
     };
 }();
@@ -46,8 +49,83 @@ class Telefono {
                 Swal.fire("No se puede proveer el servicio", "", "info");
             }
         });
-
-
-
     }
+}
+
+function confirmacion() {
+        Swal.fire({
+            title: "La reparación esta por empezar",
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: "SI",
+            denyButtonText: `NO`
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire("Saved!", "", "success");
+                arrTelefonos.push(this);
+                console.log(arrTelefonos)
+                document.getElementById("finalizado").disabled=false;
+                document.getElementById("estado").innerHTML = "Empezando";
+            } else if (result.isDenied) {
+                Swal.fire("En caso cambie de opinion, lo esperamos", "", "info");
+                document.getElementById("estado").innerHTML = "Rechazado";
+            }
+        });
+}
+
+function diagnostico(){
+    diagN=`DIAGNOSTICO: Se encontró dañado la unidad de carga, por lo que el costo seria 150 soles`;
+    alert(diagN)
+    document.getElementById("myCheck").disabled=false;
+    document.getElementById("mySelect").disabled=false;
+    document.getElementById("estado").innerHTML = "Diagnostico";
+}
+
+function Abono() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+        if (checkBox.checked == true){
+          text.style.display = "block";
+          document.getElementById("autorizacion").disabled=false;
+          document.getElementById("estado").innerHTML = "Cotizado";
+        } else {
+           text.style.display = "none";
+           document.getElementById("autorizacion").disabled=true;
+        }
+      }
+
+function seleccionMarca() {
+    var marca = document.getElementById("mySelect").value;
+    var tecn;
+switch(marca){
+    case "Samsung":
+        tecn="Jose Lopez"
+        break;
+    case "Apple":
+        tecn="Alberto Fernandez"
+        break;
+    case "Xiaomi":
+        tecn="Carlos Dominguez"
+        break;
+    case "Oppo":
+        tecn="Jesus Martinez"
+        break;
+    case "Motorola":
+        tecn="Eduardo Jimenez"
+        break;
+    case "Huawei":
+        tecn="Renzo Paredes"
+        break;
+    case "Realme":
+        tecn="Alex Galvez"
+        break;
+    default:
+        tecn="Cesar Vasquez"
+        break;
+}  
+
+  document.getElementById("tecnico").innerHTML = "El técnico especialista es: " + tecn;
+}
+function terminado(){
+    document.getElementById("estado").innerHTML = "Terminado";
 }
