40805163c0b5a509272d2d19e6ba69ba7e51b484
Roberto Pineda <rpineda@x-codec.net>
2024-01-02 21:52:46 -0500

    Clase 02012024

--- Semana07Sesion01/d19423/index.js
+++ Semana07Sesion01/d19423/index.js
@@ -1,2 +1,39 @@
-console.log("Hola desde nodejs");
-console.log("Prueba de cambio");
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+const usr = [
+    {
+        nombre: "Roberto",
+        apellido: "Pineda"
+    }, {
+        nombre: "David",
+        apellido: "Lopez"
+    }, {
+        nombre: "Karen",
+        apellido: "Lam"
+    }]
+
+app.get('/usuarios', (req, res) => {
+    res.send(usr)
+})
+app.get('/cliente', (req, res) => {
+    let nombre = req.query.nombre;
+    let respuesta;
+    usr.forEach(element => {
+        if (element.nombre == nombre){
+            respuesta = element;
+            res.send(respuesta)
+        }
+        
+    });
+    
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
