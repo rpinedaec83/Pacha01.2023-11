3c7ec247c6c26e5f6f31937246406847281f8811
Gerardo Dagnino <gerardoangecala@gmail.com>
2024-01-19 15:52:52 -0500

    realizando hackaton

---
+++ Hackaton09/SV72653452/Hackaton09.sql
@@ -0,0 +1,8 @@
+-- Ejercicio1
+Select * from Customers where country = 'USA';
+
+-- Ejercicio2
+Select * from customers where city ='BERLIN'; 
+
+-- Ejercicio3
+Select * from employees where EmployeeID in (3, 5, 8); 
