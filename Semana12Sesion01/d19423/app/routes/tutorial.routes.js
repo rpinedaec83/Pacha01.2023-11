2dd14047f3d8d8b6cfa7292728619f1d8eb9b872
Roberto Pineda <rpinedaec83@gmail.com>
2024-02-08 20:25:40 -0500

    Clase 08022024 primera parte

--- Semana12Sesion01/d19423/app/routes/tutorial.routes.js
+++ Semana12Sesion01/d19423/app/routes/tutorial.routes.js
@@ -1,6 +1,7 @@
 
 module.exports = app => {
     const tutorials = require("../controllers/tutorial.controller.js");
+    const tags = require("../controllers/tag.controller.js");
     var router = require("express").Router();
 
     router.post("/", tutorials.create);
@@ -8,6 +9,15 @@ module.exports = app => {
     router.get("/:id", tutorials.findOne);
     router.put("/:id", tutorials.update);
     router.delete("/:id", tutorials.delete);
+    router.delete("/", tutorials.deleteAll);
+    router.get("/published/:id", tutorials.findAllPublished);
+
+    router.post("/comment/:id", tutorials.createComment);
+
+    router.post("/tag", tags.create);
+    router.get("/tag", tags.findAll);
+
+    router.post("/tag/tutorial", tags.addTutorial)
 
     app.use('/api/tutorials', router);
 
