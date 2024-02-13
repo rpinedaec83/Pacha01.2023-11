7dac9748cf2521800fbc131344e993f41528da57
Rosita <rosa00nc@gmail.com>
2024-02-12 21:10:42 -0500

    hackaton

---
+++ Hackaton12/sv45846575/models/nacionalidad.model.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, DataTypes) => {
+    const Especie = sequelize.define("especie", {
+        descripcion: {
+            type: DataTypes.STRING,
+        },
+    });
+    return Especie;
+};
