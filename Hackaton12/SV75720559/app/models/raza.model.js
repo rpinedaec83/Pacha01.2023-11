e5f6928b6033ce198f4f3ecaf0858988c825c96a
Luis Chilin Capcha <luischilincapcha@gmail.com>
2024-02-25 22:17:01 -0500

    Hackaton 12 SV75720559

---
+++ Hackaton12/SV75720559/app/models/raza.model.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+    const Raza = sequelize.define("raza", {
+        descripcion: {
+            type: DataTypes.STRING,
+        },
+    });
+
+    return Raza;
+};
